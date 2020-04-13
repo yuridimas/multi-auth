3e9ff41c193f643d3d994b6b497632d42e63040d
Yuri Dimas Satrio <yuridimas00@gmail.com>
2020-04-13 15:51:04 +0700

    front: tambah refrensi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Socialite
                 </div>
 
+                Buat konfigurasinya disini dulu
+                <hr>
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://developers.google.com/" target="_blank">Google</a>
+                    <a href="https://developers.facebook.com/?locale=id_ID" target="_blank">Facebook</a>
+                    <a href="https://www.itsolutionstuff.com/post/laravel-6-socialite-login-with-google-gmail-accountexample.html" target="_blank">Refrensi</a>
                 </div>
             </div>
         </div>
